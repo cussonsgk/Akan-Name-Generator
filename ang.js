b85c88a78e50c79172f754df998e2238d8b5d8a1
cussonsgk <cussonsgk@gmail.com>
2026-02-05 20:49:14 +0300

    made my js able to get the user input

--- ang.js
+++ ang.js
@@ -1,2 +1,6 @@
 document.getElementById("form").addEventListener("submit", function (e) {
   e.preventDefault();
+  const day = document.getElementById("day").value;
+  const month = document.getElementById("month").value;
+  const year = document.getElementById("year").value;
+  const gender = document.querySelector('input[name="gender"]:checked');
