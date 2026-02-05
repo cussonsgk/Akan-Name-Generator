175be720efc0f02f81e93824f53ae480f907bd09
cussonsgk <cussonsgk@gmail.com>
2026-02-05 20:50:02 +0300

    no empty spaces

--- ang.js
+++ ang.js
@@ -4,3 +4,6 @@ document.getElementById("form").addEventListener("submit", function (e) {
   const month = document.getElementById("month").value;
   const year = document.getElementById("year").value;
   const gender = document.querySelector('input[name="gender"]:checked');
+  if (!day || !month || !year || !gender)
+    {alert("Please fill in your DOB and select your gender");
+    return;}
