f9afa8a7edc342731bc5d4254c398f707cef6b96
cussonsgk <cussonsgk@gmail.com>
2026-02-05 20:50:34 +0300

    no invalid dates

--- ang.js
+++ ang.js
@@ -7,3 +7,6 @@ document.getElementById("form").addEventListener("submit", function (e) {
   if (!day || !month || !year || !gender)
     {alert("Please fill in your DOB and select your gender");
     return;}
+  if (day<1 || day>31 || month<1 || month>12)
+    {alert("Enter a valid date");
+    return;}
